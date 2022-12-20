b3dffcb52fdf7abfeee2f7778b647c2b863868c8
Joe <doode1524@gmail.com>
2022-12-20 17:09:50 -0500

    fucking format

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import './App.css';
+import "./App.css";
 
 const App = () => {
   return (
@@ -6,6 +6,6 @@ const App = () => {
       <h1>Hellow</h1>
     </div>
   );
-}
+};
 
 export default App;
